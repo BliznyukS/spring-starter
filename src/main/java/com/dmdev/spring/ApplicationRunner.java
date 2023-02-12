5e1dbb69afe72da1ef13d55209ea5fc272b81594
Serhii Blyzniuk <bliznyuk.sergey@gmail.com>
2023-02-12 19:11:03 +0200

    6. Constructor Injection

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -5,24 +5,30 @@ import com.dmdev.spring.database.repository.CompanyRepository;
 import com.dmdev.spring.database.repository.UserRepository;
 import com.dmdev.spring.ioc.Container;
 import com.dmdev.spring.service.UserService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
 
-        var container = new Container();
+        var context = new ClassPathXmlApplicationContext("application.xml");
 
-//        var connectionPool = container.get(ConnectionPool.class);
-//        var userRepository = container.get(UserRepository.class);
-//        var companyRepository = container.get(CompanyRepository.class);
+        var connectionPool = context.getBean("pool1", ConnectionPool.class);
+        System.out.println(connectionPool);
+
+//        var container = new Container();
+//
+////        var connectionPool = container.get(ConnectionPool.class);
+////        var userRepository = container.get(UserRepository.class);
+////        var companyRepository = container.get(CompanyRepository.class);
+////        var userService = container.get(UserService.class);
+//
+////        var connectionPool = new ConnectionPool();
+////        var userRepository = new UserRepository(connectionPool);
+////        var companyRepository = new CompanyRepository(connectionPool);
+////        var userService = new UserService(userRepository, companyRepository);
+//
 //        var userService = container.get(UserService.class);
-
-//        var connectionPool = new ConnectionPool();
-//        var userRepository = new UserRepository(connectionPool);
-//        var companyRepository = new CompanyRepository(connectionPool);
-//        var userService = new UserService(userRepository, companyRepository);
-
-        var userService = container.get(UserService.class);
 
     }
 }
