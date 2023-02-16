0f2eb37ee7f32a026f1c2bdc41a4caa243de76cb
Serhii Blyzniuk <bliznyuk.sergey@gmail.com>
2023-02-16 20:51:15 +0200

    checked with lessons example

--- education/src/main/java/com/dmdev/spring/database/pool/ConnectionPool.java
+++ education/src/main/java/com/dmdev/spring/database/pool/ConnectionPool.java
@@ -1,11 +1,13 @@
 package com.dmdev.spring.database.pool;
 
+import org.springframework.beans.factory.InitializingBean;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.util.List;
 import java.util.Map;
 
-public class ConnectionPool {
+public class ConnectionPool implements InitializingBean {
 
     private final String username;
     private final Integer poolSize;
@@ -25,10 +27,17 @@ public class ConnectionPool {
     public void setProperties(Map<String, Object> properties) {
         this.properties = properties;
     }
+
     @PostConstruct
     private void init() {
         System.out.println("Init connection pool");
     }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Properties set");
+    }
+
     @PreDestroy
     private void destroy() {
         System.out.println("Clean connection pool");
