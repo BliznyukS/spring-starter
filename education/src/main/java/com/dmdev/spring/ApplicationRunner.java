b07f4c13f0a0f60457d4feb0108b6ebe7dfe6c47
Serhii Blyzniuk <bliznyuk.sergey@gmail.com>
2023-02-19 14:29:01 +0200

    YAML done

--- education/src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ education/src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -1,9 +1,12 @@
 package com.dmdev.spring;
 
+import com.dmdev.spring.config.DatabaseProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ApplicationRunner {
 
     public static void main(String[] args) {
@@ -11,6 +14,7 @@ public class ApplicationRunner {
         var context = SpringApplication.run(ApplicationRunner.class, args);
         System.out.println(context.getBeanDefinitionCount());
         System.out.println(context.getBean("pool1"));
+        System.out.println(context.getBean(DatabaseProperties.class));
 
     }
 }
